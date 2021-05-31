ce20aeaae25133ae5093e6ade2a25cdae6e654be
hilaa <hilaa@DESKTOP-IH7ML77>
2021-05-31 19:08:33 +0300

    19:08

--- src/geometries/Geometry.java
+++ src/geometries/Geometry.java
@@ -1,5 +1,6 @@
 package geometries;
 
+import primitives.Color;
 import primitives.Point3D;
 import primitives.Vector;
 
@@ -7,7 +8,27 @@ import primitives.Vector;
  * interface for all the geometries that have a normal from them
  */
 public abstract class Geometry implements Intersectable{
+	
+	protected Color emission=Color.BLACK;   
+	
     /**
+	 * @return the emission
+	 */
+	public Color getEmmission() {
+		return emission;
+	}
+
+
+	/**
+	 * @param emmission the emission to set
+	 */
+	public Geometry setEmission(Color _emission) {
+		this.emission = _emission;
+		return this;
+	}
+
+
+	/**
      *
      * @param point should be null for flat geometries
      * @return the normal to the geometry
